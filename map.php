5da83c8b0ef96c77c13f10adf4a1fcf71434644a
Blair Tinker <btinker@library.rochester.edu>
2015-06-15 15:25:57 -0400

    Added comments and cleaned up a bit

--- map.php
+++ map.php
@@ -2,19 +2,19 @@
 <!doctype html>
 <html lang="en">
   <head>
-     <link rel="stylesheet" href="http://openlayers.org/en/v3.1.0/css/ol.css" type="text/css">
-     <link rel="stylesheet" href="neatline_mapper.css" type="text/css">
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/ol3/3.5.0/ol.js"></script>
+    <link rel="stylesheet" href="http://openlayers.org/en/v3.1.0/css/ol.css" type="text/css">
+    <link rel="stylesheet" href="neatline_mapper.css" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ol3/3.5.0/ol.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <title>Introduction to Modern Architecture</title>
   </head>
-  <body>
-
+  
+<body>
 <div id="loadingcont">
 <div id="loading">LOADING</div>
-
 </div>
-    <div id="map" class="map">
+      
+<div id="map" class="map">
     <div id="title"> <div id="title_box">  Introduction to Modern Architecture</div></div>
 <div id="titlepopup">
     <div id="titlecontent"></div>
@@ -25,36 +25,41 @@
     <div id="popcontent"></div>
 </div>
 </div>
+    
 <div id="layercontainer">
     <div id="min" title="Hide Layers">-</div>
 <div id="max" title="Show Layers">+</div>
 <div id="layerlist">
     <div class="layer_heading">Base Layers</div>
-      <input type="radio" name="base" value="OSM" class="base" checked="true">Open Streetmap<br>
-      <input type="radio" name="base" value="esri" class="base">ESRI<br>
-    <input type="radio" name="base" value="wc" class="base" >Stamen Watercolor<br>
-    <input type="radio" name="base" value="toner" class="base">Stamen Toner<br>
+        <input type="radio" name="base" value="OSM" class="base" checked="true">Open Streetmap<br>
+        <input type="radio" name="base" value="esri" class="base">ESRI<br>
+        <input type="radio" name="base" value="wc" class="base" >Stamen Watercolor<br>
+        <input type="radio" name="base" value="toner" class="base">Stamen Toner<br>
     <div class="layer_heading">Exhibits</div>
   </div>
+</div>
+
 </div>
     
     <script type="text/javascript">
 
 $(document).ready(function(){
 
+//timer set up to let the page load
 var tmp = setTimeout(function(){
   $('#loading').hide();
    $('#loadingcont').hide();
-}, 500);
+}, 2500);
 
- $( "#titlepopup" ).hide();
-
-    $( "#infopopup" ).hide();
-    $( "#max" ).hide();
-    
+//hide the divs that will hold the title and the information popups
+$( "#titlepopup" ).hide();
+$( "#infopopup" ).hide();
+$( "#max" ).hide();
+ 
+//function to handle the switching of the base layers 
     $( ".base" ).change(function() {
   
-  switch($( "input:radio[name=base]:checked" ).val()) {
+    switch($( "input:radio[name=base]:checked" ).val()) {
     case "esri":
         ESRI.setVisible(1);
         OSM.setVisible(0);
@@ -86,11 +91,13 @@ var tmp = setTimeout(function(){
     
     var ed = [];
     
+    //attibution giving esri credit for their data
     var attribution = new ol.Attribution({
   html: 'Tiles &copy; <a href="http://services.arcgisonline.com/ArcGIS/' +
       'rest/services/World_Topo_Map/MapServer">ArcGIS</a>'
 });
     
+    //configure the ESRI layer
     var ESRI = new ol.layer.Tile({
       source: new ol.source.XYZ({
         attributions: [attribution],
@@ -100,6 +107,7 @@ var tmp = setTimeout(function(){
 	visible: false
     });
         
+        //configure the stamen toner layer
         var toner = new ol.layer.Tile({
       source: new ol.source.Stamen({
         layer: 'toner-lite'
@@ -107,6 +115,7 @@ var tmp = setTimeout(function(){
 	visible: false
     });
         
+        //configure the stamen watercolor layer
         var WC = new ol.layer.Tile({
       source: new ol.source.Stamen({
         layer: 'watercolor'
@@ -114,25 +123,28 @@ var tmp = setTimeout(function(){
 	visible: false
     });
     
+        //configure the stamen Open Street Map layer
         var OSM = new ol.layer.Tile({
         source: new ol.source.OSM(),
         name: "raster",
         exhibitname: "na",
 	visible: true 
         });
-        
+    
+//set up the initial map view    
 var view = new ol.View({
   center: [0, 5000000],
   zoom: 3
 });
 
+//set up the map and add the base layers
 var map = new ol.Map({
   layers: [WC, toner, OSM, ESRI],
   target: 'map',
   view: view
 });
 
-
+//set up the feature overlay  and styles so that feature highlight properly
 var featureOverlay = new ol.FeatureOverlay({
   map: map,
   style:  function(feature) {
@@ -167,7 +179,8 @@ var featureOverlay = new ol.FeatureOverlay({
 }
 });
 
- //adding some comments
+ //setting up the style function for the features.  This gets the values from the features and uses it 
+ //to style the features
 var styleFunction = function(feature) {
     var hexColor = feature.get('fill_color');
     var color = ol.color.asArray(hexColor);
@@ -179,7 +192,6 @@ var styleFunction = function(feature) {
       color: color
     }),
 
-
   image: new ol.style.Circle({
       radius: 10, 
 		
@@ -187,7 +199,6 @@ var styleFunction = function(feature) {
       color: feature.get('stroke_color')
     }),
 
-	
 	fill: new ol.style.Fill({
         color: color 
       }), 
@@ -199,34 +210,10 @@ var styleFunction = function(feature) {
   })];
 };
 
-  
-  function newLayerAdded(layerName, desc, layer) {
-    var new_layer = document.createElement( 'div' );
-    new_layer.setAttribute("class", "layer");
-    new_layer.setAttribute("id", layerName);
-    var new_checkbox = document.createElement("input");
-    new_checkbox.setAttribute("type", "checkbox");
-    new_checkbox.setAttribute("id", layerName);
-    new_checkbox.setAttribute("class", "layer_check");
-    var new_layerlabel = document.createTextNode(layerName);
-    $( "#layerlist" ).append(new_checkbox);
-    new_layer.appendChild(new_layerlabel);
-    
-    $( "#layerlist" ).append(new_layer);
-    var visible = $(new_checkbox);
-    visible.on('change', function() {
-    layer.setVisible(this.checked);
-  });
-  visible.prop('checked', layer.getVisible());
-  }
-  
-    $.getJSON( "get_layers.php", function( data ) {
-        
-        //console.log("data recieved");
-        $.each( data, function( key, val ) {
+    $.getJSON( "get_layers.php", function( data ) {//call the script that will get the json from the mysql database
+        $.each( data, function( key, val ) { //loop through the 
                 $url = "get_records.php?eid[]=" + val["id"];
                 $.get($url, function( NeatlineExhibits) {
-                  console.log (NeatlineExhibits);
                     
                     var vectorSource = new ol.source.Vector({
                   features: (new ol.format.GeoJSON()).readFeatures(NeatlineExhibits)
@@ -247,7 +234,26 @@ var styleFunction = function(feature) {
             });    
     });
     
+      //This function adds 
+  function newLayerAdded(layerName, desc, layer) {
+    var new_layer = document.createElement( 'div' );
+    new_layer.setAttribute("class", "layer");
+    new_layer.setAttribute("id", layerName);
+    var new_checkbox = document.createElement("input");
+    new_checkbox.setAttribute("type", "checkbox");
+    new_checkbox.setAttribute("id", layerName);
+    new_checkbox.setAttribute("class", "layer_check");
+    var new_layerlabel = document.createTextNode(layerName);
+    $( "#layerlist" ).append(new_checkbox);
+    new_layer.appendChild(new_layerlabel);
     
+    $( "#layerlist" ).append(new_layer);
+    var visible = $(new_checkbox);
+    visible.on('change', function() {
+    layer.setVisible(this.checked);
+  });
+  visible.prop('checked', layer.getVisible());
+  }
     
       $("#closer").click(
         function(){
@@ -274,7 +280,7 @@ var styleFunction = function(feature) {
     //var tmp = setTimeout(function(){ 
     $( "#layerlist").on({
     mouseenter: function () {
-      if (ed[this.id] && $('#infopopup').css('display') == "none") {
+      if (ed[this.id] && $('#infopopup').css('display') === "none") {
        $( "#titlepopup" ).show();
 	 $( "#titlecontent" ).html(ed[this.id]);
       }
@@ -303,16 +309,12 @@ var displayTitleInfo = function(pixel) {
     highlight = feature;
   }
 
-
-
     if (feature){
-	 if(feature.get('fid') && $('#infopopup').css('display') == "none"){
+	 if(feature.get('fid') && $('#infopopup').css('display') === "none"){
          $( "#titlepopup" ).show();
 	 $( "#titlecontent" ).html(feature.get('item_title'));
 	/////////////
         }
-
-	/////////////////
 
 	}else{
 //featureOverlay.removeFeature(highlight);
@@ -320,8 +322,7 @@ var displayTitleInfo = function(pixel) {
         $( "#titlepopup" ).hide();
 }
 
-
-}
+};
 
 
 //get feature info on hover
