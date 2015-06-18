bfca7c0a71a813bc900581d9711f2987bf84d0e1
Blair Tinker <btinker@library.rochester.edu>
2015-06-18 16:10:24 -0400

    added handlers for image icons used for point features

--- style_functions.js
+++ style_functions.js
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function getColor(sel){
+    
+}
 
 //set up the feature overlay  and styles so that feature highlight properly
 var featureOverlay = new ol.FeatureOverlay({
@@ -12,20 +15,29 @@ var featureOverlay = new ol.FeatureOverlay({
         var color = ol.color.asArray(hexColor);
         color = color.slice();
         color[3] = feature.get('fill_opacity_select'); // change the alpha of the color
-
-        return [new ol.style.Style({
-                fill: new ol.style.Fill({
-                    color: color
-                }),
-                image: new ol.style.Circle({
-                    radius: 10,
+      
+        //this conditional tests if the feature is represented by an image file and sets 
+        //the style appropriately
+        if (feature.get('point_image')) {
+            myImg = "";
+        }else{
+            myImg = new ol.style.Circle({
+                    radius:feature.get('point_radius'),
                     stroke: new ol.style.Stroke({
                         color: feature.get('stroke_color_select')
                     }),
                     fill: new ol.style.Fill({
                         color: color
+                       
                     }),
+                });
+            }
+            
+        return [new ol.style.Style({
+                fill: new ol.style.Fill({
+                    color: color
                 }),
+                image:myImg,
                 stroke: new ol.style.Stroke({
                     color: feature.get('stroke_color_select')
                 })
@@ -40,20 +52,38 @@ var styleFunction = function (feature) {
     var color = ol.color.asArray(hexColor);
     color = color.slice();
     color[3] = feature.get('fill_opacity'); // change the alpha of the color
+    console.log(color);
+    //this conditional tests if the feature is represented by an image file and sets 
+    //the style appropriately
+    if (feature.get('point_image')) {
+        var myImg = new Image();
+        myImg.src = feature.get('point_image');
+        var myImage = new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            opacity: color[3],
+            offset: [0, 0],
+            scale: 1,
+            imgSize: [feature.get('point_radius'), feature.get('point_radius')],
+            img: myImg
+        }));
+
+        console.log(feature.get('point_image'));
+    } else {
+        myImage = new ol.style.Circle({
+            radius: feature.get('point_radius'),
+            stroke: new ol.style.Stroke({
+                color: feature.get('stroke_color')
+            }),
+            fill: new ol.style.Fill({
+                color: color
+            }),
+        });
+    }
 
     return [new ol.style.Style({
             fill: new ol.style.Fill({
                 color: color
             }),
-            image: new ol.style.Circle({
-                radius: 10,
-                stroke: new ol.style.Stroke({
-                    color: feature.get('stroke_color')
-                }),
-                fill: new ol.style.Fill({
-                    color: color
-                }),
-            }),
+            image: myImage,
             stroke: new ol.style.Stroke({
                 color: feature.get('stroke_color')
             })
