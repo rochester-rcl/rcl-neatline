86935b9c458c9b0f52f59d8d1856fd4189d032f5
Blair Tinker <btinker@library.rochester.edu>
2015-12-11 10:40:39 -0500

    Timeline is mostly working but is not flexible.  Changed the get_layers.php a bit to create more valid JSON response.

--- get_layers.php
+++ get_layers.php
@@ -26,6 +26,12 @@ try {
     $sel = "SELECT DISTINCT id,title,narrative FROM omeka_neatline_exhibits WHERE id IN ($evstring)";
     $stmt = $conn->query($sel);
     $row1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   
+    foreach ($row1 as $k => &$v) {
+        foreach ($v as $k1 => &$v1){
+            $v1 = escapeJsonString($v1);
+        }
+}
     echo json_encode($row1);
 }
     
@@ -34,4 +40,19 @@ catch(PDOException $e)
     echo "Error: " . $e->getMessage();
     }
     
+    function escapeJsonString($value) { # list from www.json.org: (\b backspace, \f formfeed)
+    $escapers = array("\\", "\"", "\n", "\r", "\t", "\x08", "\x0c");
+    $replacements = array("\\\\", "\\\"", "", "", "\\t", "\\f", "\\b");
+    $result = str_replace($escapers, $replacements, $value);
+    return $result;
+}
+
+function debug_to_console( $data ) {
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
     ?>
